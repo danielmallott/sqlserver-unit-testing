3d2a0e9b2e8e97451647d97b9eb87258c0571582
Dan Mallott <daniel.mallott@visma.com>
2023-09-12 11:52:59 +0200

    Update run-tests.ps1 after directory structure rearrange

--- src/run-tests.ps1
+++ src/run-tests.ps1
@@ -1,3 +1,3 @@
-Get-ChildItem -Path ./StackOverflow/Tests -Force | ForEach-Object -Process {
+Get-ChildItem -Path ./src/Tests/WideWorldImporters -Force | ForEach-Object -Process {
     Invoke-Sqlcmd -InputFile $_.FullName -ConnectionString "Data Source=localhost;Initial Catalog=WideWorldImporters;User Id=sa;Password=Password123!;TrustServerCertificate=true"
 }
